3736889f7772b81d565aa343e8a5d611fee320bc
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-06-10 09:19:56 -0300

    adicionando projetos

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,10 +6,10 @@ export const slideData = [
         description: "BarberPro é um sistema de gerenciamento para barbearias que permite administrar barbeiros, agendamentos, horários e pagamentos de forma prática e eficiente. A plataforma oferece controle total da agenda, facilitando o acompanhamento dos serviços e otimizando o atendimento",
         type: "Full-Stack",
         src: "/projects/barberpro/BarberProIcone.png",
-        techStack: ["React", "Next.js", "TailwindCSS", "Zod", "Shadcn-ui", "Node.js", "Prisma"],
-        images: ["/projects/barberpro/login-barbearia.png", "/projects/barberpro/home.png", 
-            "/projects/barberpro/agendamento-1.png", "/projects/barberpro/agendamento-2.png", 
-            "/projects/barberpro/barbeiros-1.png", "/projects/barberpro/barbeiros-2.png", 
+        techStack: ["React", "Next.js", "TailwindCSS", "Zod", "Shadcn-ui", "Node.js", "Prisma", "Responsivo"],
+        images: ["/projects/barberpro/login-barbearia.png", "/projects/barberpro/home.png",
+            "/projects/barberpro/agendamento-1.png", "/projects/barberpro/agendamento-2.png",
+            "/projects/barberpro/barbeiros-1.png", "/projects/barberpro/barbeiros-2.png",
             "/projects/barberpro/barbearia-1.png", "/projects/barberpro/barbearia-2.png"],
         github: {
             frontend: "https://github.com/jooluisrm/barber-pro-barbearia",
@@ -18,7 +18,48 @@ export const slideData = [
         deploy: {
             link: "https://barber-pro-barbearia.vercel.app"
         },
-        button: "View More"
+        button: "Ver mais"
+    },
+    {
+        title: "BarberPro - Cliente",
+        description: "BarberPro é uma plataforma de agendamento online para barbearias, permitindo que clientes escolham sua barbearia, barbeiro e horário de atendimento de forma prática e rápida. O sistema também exibe produtos disponíveis para venda na barbearia. ",
+        type: "Full-Stack",
+        src: "/projects/barberpro/BarberProIcone.png",
+        techStack: ["React", "Next.js", "TailwindCSS", "Zod", "Shadcn-ui", "Node.js", "Prisma", "Responsivo"],
+        images: ["/projects/barberpro-cliente/home.png","/projects/barberpro-cliente/home-1.png",
+            "/projects/barberpro-cliente/barbearia.png","/projects/barberpro-cliente/barbearia-1.png","/projects/barberpro-cliente/barbearia-2.png",
+            "/projects/barberpro-cliente/barbearia-3.png","/projects/barberpro-cliente/barbearia-4.png",
+            "/projects/barberpro-cliente/buscar.png","/projects/barberpro-cliente/agendamentos.png",
+            "/projects/barberpro-cliente/perfil.png",
+         ],
+        github: {
+            frontend: "https://github.com/jooluisrm/barber-pro-front-end",
+            backend: "https://github.com/jooluisrm/barber-pro-backend"
+        },
+        deploy: {
+            link: "https://barber-pro-front-end.vercel.app/"
+        },
+        button: "Ver mais"
+    },
+    {
+        title: "TMDBFLIX",
+        description: "TMDBFlix é um projeto desenvolvido com React e Next.js que utiliza a API do TMDB (The Movie Database) para exibir informações detalhadas sobre filmes e séries. Com o TMDBFlix, é possível consultar dados como sinopse, elenco, avaliação, data de lançamento e muito mais.",
+        type: "Front-end",
+        src: "/tmdb.jpg",
+        techStack: ["React", "Next.js", "TailwindCSS", "Shadcn-ui", "Axios"],
+        images: ["/projects/tmdbflix/home.png","/projects/tmdbflix/home-1.png",
+            "/projects/tmdbflix/busca.png","/projects/tmdbflix/serie.png",
+            "/projects/tmdbflix/serie-1.png","/projects/tmdbflix/serie-2.png",
+            "/projects/tmdbflix/serie-3.png","/projects/tmdbflix/serie-4.png",
+            "/projects/tmdbflix/filme.png",
+         ],
+        github: {
+            frontend: "https://github.com/jooluisrm/tmdb"
+        },
+        deploy: {
+            link: "https://tmdbflix.vercel.app/"
+        },
+        button: "Ver mais"
     },
 
 ];
